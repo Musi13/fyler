1393effa7bdf85194cb8f9c3ae6a4df246e82af4
Alex Gonzales <musigonzales@gmail.com>
2020-07-27 21:01:35 -0500

    Fix package description, have to bump version for no changes...

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.8
 from setuptools import setup, find_packages
+from pathlib import Path
 
 # Package meta-data.
 DESCRIPTION = 'An application to help organize TV Shows/Anime/Movies'
@@ -19,12 +20,16 @@ REQUIRED = [
     'diskcache',
 ]
 
+with open(Path(__file__).parent / 'README.md') as f:
+    long_description = f.read()
+
 setup(
     name='fyler',
-    version='0.1.0',
+    version='0.1.1',
     description=DESCRIPTION,
-    long_description=DESCRIPTION,
-    author='Alex Gonzales<musigonzales@gmail.com>',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alex Gonzales',
     python_requires='>=3.8.0',
     packages=find_packages(exclude=['tests']),
     package_data={'fyler.assets': ['*.ui']},
